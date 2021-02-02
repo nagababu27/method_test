c35d9fbb13a19c4c6a45fa1559a57060859c6da4
unknown <nagababumulukuri@gmail.com>
2021-02-02 13:19:48 +0530

    first commit

---
+++ src/app/_model/bindModel.ts
@@ -0,0 +1,6 @@
+export class bindModel {
+     id: Number;
+    name: String;
+    email: String;
+    gender: String;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
-
+import bindData from './bindData.json';
+import {bindModel} from './_model/index';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+ 
 export class AppComponent {
-  title = 'methhub';
+	bindData :any=[]
+	status:any=false;
+	imageStatus:any;
+	loopData:any=[];
+	
+
+	constructor(){
+	}
+	  ngOnInit() {
+	  	this.getAppendDatya()
+	   }
+
+
+	 getAppendDatya() {
+	 	this.bindData= bindData;
+	 	this.loopData=[
+	 	{"id":1,"url":'../assets/img/dummy.jpg'},
+	 	{"id":2,"url":'../assets/img/dummy.jpg'},
+	 	{"id":3,"url":'../assets/img/dummy.jpg'},
+	 	{"id":4,"url":'../assets/img/dummy.jpg'},
+	 	{"id":5,"url":'../assets/img/dummy.jpg'},
+	 	{"id":6,"url":'../assets/img/dummy.jpg'}
+	 	];
+	 	
+	 }
+	
+	addImage(image){
+        for(let i=0;i<this.bindData.length;i++){
+        	if(this.bindData[i].id == image.id){
+        		this.status=true;
+        		this.loopData[i]["url"]=this.bindData[i]["url"];
+        	}
+        }
+	}
+
+
+	deleteImage(image){
+		
+       console.log(this.removeUrlImage(image))
+		
+
+	}
+	removeUrlImage(image){
+		// for(let i=0;i<this.bindData.length;i++){
+  //       	if(this.bindData[i].id == image.id){      		
+		// 		  this.loopData[i].url='../assets/img/dummy.jpg';
+		// 		  console.log(this.loopData)
+		// 	}
+
+        // }
+			const index =this.loopData.findIndex((el) => el.id ===image.id)
+
+			this.loopData[index]='';
+			console.log(this.loopData.reverse().sort())
+			return this.loopData.reverse()
+
+	}
 }
